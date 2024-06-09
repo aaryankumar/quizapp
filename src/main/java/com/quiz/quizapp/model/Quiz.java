c62fe0d8dda8c628991ca3e819b72d45cbf4c6b6
aaryan kumar <aaryankumarhmhs@gmail.com>
2024-06-10 03:35:00 +0530

    3rd cimmit to mybranch

---
+++ src/main/java/com/quiz/quizapp/model/Quiz.java
@@ -0,0 +1,18 @@
+package com.quiz.quizapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Data
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+
+    @ManyToMany
+    private List<Question> questions;
+}
